b4081ea37a9066504aa02fda7d9b96185f0db658
vishall <65184972+vpatel1816@users.noreply.github.com>
2020-05-28 16:00:05 -0400

    with blog page and calulator page

--- CalcAppWithLogin/newapp/blog/admin.py
+++ CalcAppWithLogin/newapp/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import BlogDetails
 # Register your models here.
+
+class Blog(admin.ModelAdmin):
+    list_display = ('blog_title','blog_author')
+
+
+admin.site.register(BlogDetails, Blog)
--- CalcAppWithLogin/newapp/blog/models.py
+++ CalcAppWithLogin/newapp/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class BlogDetails(models.Model):
+    blog_title = models.CharField(max_length=30)
+    blog_details = models.TextField(max_length=200)
+    date = models.DateField()
+    time = models.TimeField()
+    blog_author = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.blog_title
