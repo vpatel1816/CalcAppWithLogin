5d0c9356e386bd633466391704e156be63a349a0
vishall <65184972+vpatel1816@users.noreply.github.com>
2020-05-28 17:47:26 -0400

    Added new Blog App

--- CalcAppWithLogin/newapp/blog/views.py
+++ CalcAppWithLogin/newapp/blog/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import BlogDetails
+from datetime import date
 
 # Create your views here.
 def blog(request):
-        return render(request, 'blog/blog.html')
+        if request.method == 'POST':
+                blog_title = request.POST.get('blog_title')
+                blog_author = request.POST.get('blog_author')
+                blog_details = request.POST.get('blog_details')
+
+                c = BlogDetails(blog_title=blog_title,blog_author=blog_author,blog_details=blog_details, date='2020-12-12', time ='18:20')
+
+                c.save()
+                blog = BlogDetails.objects.all
+                return render(request, 'blog/blog.html', {'blog': blog})
+        else:
+                blog = BlogDetails.objects.all
+                return render(request, 'blog/blog.html', {'blog' : blog})
